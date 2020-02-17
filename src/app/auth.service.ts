2d821c9dad7d3563b460f074740290e49fc71270
R@HUL <31180528+guntharahul@users.noreply.github.com>
2020-02-17 20:59:38 +0530

    Login and Auth service
    Added Login page and authentication service

---
+++ src/app/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {of} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+  login(email: string, password: string) {
+    const loginCredentials = {email, password}
+    console.log('User login credentials', {email, password});
+    return of({email, password});
+  }
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'pm-login',
@@ -9,11 +10,14 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
   email: string;
   password: string;
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
   ngOnInit() {
   }
   login() {
-    this.router.navigate(['']);
+    this.authService
+      .login(this.email, this.password).subscribe(s => {
+        this.router.navigate(['']);
+    });
   }
 }
